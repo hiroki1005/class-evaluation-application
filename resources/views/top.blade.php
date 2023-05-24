7db783a3124c1467c97875d969e066613c098a31
hiroki1005 <matsunaga.hiroki1005@gmail.com>
2023-05-24 17:42:43 +0900

    初期設定&基本実装

---
+++ resources/views/top.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="/css/style.css">
+
+    </head>
+    
+    <body>
+     <div class="headline">
+        <h1>Kindai Evaluation</h1>
+        <p>近大生のための授業評価サイト</p>
+     </div>
+     
+      <div class="item">
+         <h2>東大阪キャンパス</h2>
+         <p>・法学部　・建築学部　・薬学部<br>
+            ・文学部　・国際学部 ・情報学部<br>
+            ・経済学部　・経営学部　・文芸学部<br>
+            ・総合社会学部　・短期大学部
+         </p>
+      </div>
+      <div class="item">                  
+         <h2>奈良キャンパス</h2>
+         <p>・農学部</p>
+      </div>
+      <div class="item">
+         <h2>広島キャンパス</h2>
+         <p>・理工学部</p>
+      </div>
+      <div class="item">
+         <h2>大阪狭山キャンパス</h2>
+         <p>・医学部</p>
+      </div>
+      <div class="item">
+         <h2>和歌山キャンパス</h2>
+         <p>・生物理工学部</p>
+      </div>
+      <div class="item">
+         <h2>福岡キャンパス</h2>
+         <p>・産業理工学部</p>
+      </div>
+      
+      <div class="image">
+         
+      </div>
+     
+    
+    </body>
